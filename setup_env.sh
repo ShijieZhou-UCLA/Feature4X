15d78299ae5397b95a6102d3cf82a5f853c2f82c
rhfeiyang <rhfeiyang@qq.com>
2025-10-17 20:05:08 -0500

    update env

--- setup_env.sh
+++ setup_env.sh
@@ -16,7 +16,7 @@ pip install torch-cluster -f https://data.pyg.org/whl/torch-2.4.1+cu124.html
 pip install torch_geometric
 
 pip install lib_render/gs3d/simple-knn
-pip install lib_render/gs3d/gof-diff-gaussian-rasterization
+#pip install lib_render/gs3d/gof-diff-gaussian-rasterization
 pip install lib_render/gs3d/diff_gaussian_rasterization-alphadep-feature
 
 pip install transformers==4.46.0 #git+https://github.com/huggingface/transformers.git
